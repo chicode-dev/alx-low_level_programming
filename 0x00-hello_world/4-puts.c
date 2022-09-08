300a7599f99fbfcd2bbd28e4c96e7e36fbfba1ce
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-08 13:01:29 -0700

    fixed indentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
-#include <string.h>
-
-/**
- *using puts function
- *using betty style
- *not a good style
- *
- * main - Entry point
- *
- * return: Always 0 (Success)
- */
-
-int main(void)
-{
-  char str[] = "\"Programming is like building a multilingual puzzle";
-  puts(str);
-  return (0);
-}
+#include <stdio.h>                                                                                                          
+#include <string.h>                                                                                                         
+                                                                                                                            
+/**                                                                                                                         
+ *using puts function                                                                                                       
+ *using betty style                                                                                                         
+ *not a good style                                                                                                          
+ *                                                                                                                          
+ * main - Entry point                                                                                                       
+ *                                                                                                                          
+ * return: Always 0 (Success)                                                                                               
+ */                                                                                                                         
+                                                                                                                            
+int main(void)                                                                                                              
+{                                                                                                                         
+	char str[] = "\"Programming is like building a multilingual puzzle";                                               
+	puts(str);                                                                                                         
+	return (0);
+}	
+	      
