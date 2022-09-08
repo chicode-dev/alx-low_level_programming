a824233962fca3c8cd240c31710448418593ef94
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-08 13:12:11 -0700

    fixed indentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,15 @@
-#include <stdio.h>                                                                                                          
-#include <string.h>                                                                                                         
-                                                                                                                            
-/**                                                                                                                         
- *using puts function                                                                                                       
- *using betty style                                                                                                         
- *not a good style                                                                                                          
- *                                                                                                                          
- * main - Entry point                                                                                                       
- *                                                                                                                          
- * return: Always 0 (Success)                                                                                               
- */                                                                                                                         
-                                                                                                                            
-int main(void)                                                                                                              
-{                                                                                                                         
-	char str[] = "\"Programming is like building a multilingual puzzle";                                               
-	puts(str);                                                                                                         
+#include <stdio.h>
+
+/**
+ * betty format style
+ *
+ * main - well its the main function
+ *
+ * return: should be 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
-	      
+}
+
