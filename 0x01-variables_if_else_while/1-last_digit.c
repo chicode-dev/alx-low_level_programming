b1bfc3482286c70b4b061282aa1b6b8334f738d0
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-11 04:42:40 -0700

    last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - last digit of a number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int l;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* a number modoulo 10 gives last digit of the number */
+	l = n % 10;
+
+	if (l > 5)
+	{
+		char c[] = "and is greater than 5";
+	}
+	else if (l == 0)
+	{
+		char c[] = "and is 0";
+	}
+	else
+	{
+		char c[] = "and is less than 6 and not 0";
+	}
+	printf("Last digit of %d is %c\n", n, c);
+	return (0);
+
