a09bafa5fbfb8b37b708850f38420779e18b1a2f
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-12 04:05:23 -0700

    print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print combo of numbers no repeats
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		for (j = 48; j <= 57; ++j)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
+
