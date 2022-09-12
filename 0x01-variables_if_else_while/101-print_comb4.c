92304be052543db775015828038f4e0cebd8b537
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-12 07:13:23 -0700

    combo of 3 numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - combo of 3 numbers print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	
+	for (i = 48; i <= 57; ++i)
+	{
+		for (j = 48; j <= 57; +j)
+		{
+			for (k = 48; k <= 57; ++k)
+			{
+				if (j != i && k != j && i != k && i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == 55 && j == 56 && k == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
