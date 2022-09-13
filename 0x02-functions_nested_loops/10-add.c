65d6d78e5148ee3a02496ccfb6c8bd96c0483e03
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-13 14:31:15 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a:first parameter
+ * @b: second parameter
+ * Return: added value
+ */
+
+int add(int a, int b)
+{
+	int add;
+	
+	add = a + b;
+	return (add);
+}
