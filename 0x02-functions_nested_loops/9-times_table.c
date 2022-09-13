2f31a9aa4c6f89ab424077da2b71dc68dd8af4c5
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-13 13:34:59 -0700

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,13 +10,17 @@ void times_table(void)
 	int j;
 	int k;
 
-	for (j = 48; j <= 57; j++)
+	for (j = 0; j <= 9; j++)
 	{
-		for (k = 48; k <= 57; k++)
+		_putchar('0');
+		for (k = 0; k <= 9; k++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			int product = j * k;
 
-			_putchar(product);
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
