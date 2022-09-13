ecdfba7c4d168b8931baab814fa8ff435cc75bd7
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-13 08:22:58 -0700

    print alphabet 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return:Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int n = 0;
+
+	while (n < 10)
+	{
+		for (i = 65; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		n++
+	}
+}
