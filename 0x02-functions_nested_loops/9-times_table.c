2237e19b35c9a87570abceeb0fb39752c2f42145
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-13 13:46:46 -0700

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 			else
 			{
 				/* first digit print */
-				_putchar((product / 10) + '0')
+				_putchar((product / 10) + '0');
 			}
 			/*last digit print */
 			_putchar((product % 10) + '0');
