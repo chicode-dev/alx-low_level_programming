9222685410e203bd8d555bce9a87e25d09656dc7
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-13 14:49:56 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,11 @@
 
 void print_to_98(int n)
 {
-	int i;
+int i;
 
-	for (i = n; i <=98; i++)
+if (n <= 98)
+{
+	for (i = n; i <= 98; i++)
 	{
 		if (i < 98)
 		{
@@ -21,5 +23,20 @@ void print_to_98(int n)
 			printf("%d", i);
 		}
 	}
-	putchar('\n');
+}
+else
+{
+	for (i = n; i >= 98; i--)
+        {
+                if (i > 98)
+                {
+                        printf("%d, ", i);
+                }
+                else
+                {
+                        printf("%d", i);
+                }
+        }
+}
+putchar('\n');
 }
