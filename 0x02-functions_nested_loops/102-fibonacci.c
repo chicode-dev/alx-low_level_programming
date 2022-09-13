c1995626e14a26d6bd03d18e9bc99f84da59c4ce
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-13 15:51:32 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci numbers
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	int f;
+	int n1 = 1;
+	int n2 = 2;
+	int n3;
+
+	printf("%d, %d, ", n1, n2);
+	while (n < 51)
+	{
+		n3 = n1 + n2;
+		printf("%d, ", n3);
+		n1 = n2;
+		n2 = n3;
+		n++;
+	}
+	return (0);
+}
