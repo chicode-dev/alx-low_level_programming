71f0f5b985a62314e84338a838c09d11c265dfa7
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-13 15:22:14 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio>
+
+/**
+ * main - natural numbers
+ * Return: sum of numberrs
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	return (sum);
+}
