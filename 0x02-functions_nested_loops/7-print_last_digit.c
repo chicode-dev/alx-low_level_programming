8a6a963e72bf32d2d160ee0e71c7af49276dd947
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-13 09:58:42 -0700

    abs

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: integer n
+ * Return: value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int l = n % 10;
+
+	return (l);
+}
