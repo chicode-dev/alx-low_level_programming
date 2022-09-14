c615050d7dbb54ac9774e49b246b5a39b568dc48
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-14 10:10:08 -0700

    debug + or -

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * main - test function
+ * 
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postitive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+debbugging c files
