6364f96f66bb63c7fdd5b11ddff6502245eb791a
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 06:05:24 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if int is uppercase
+ *
+ * Return: 1 (success) and 0 (failure)
+ */
+
+int _isupper(int c)
+{
+	int a;
+
+	for (a = 65; a < 100; a++)
+	{
+		if (c == a)
+		{
+			Return (1);
+		}
+	}
+	return (0);
+}
