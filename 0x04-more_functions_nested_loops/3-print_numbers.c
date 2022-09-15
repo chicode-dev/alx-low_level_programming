21f87a83cef4add4a1379c373ced26705f76cd83
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 09:18:53 -0700

    0to9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ *
+ * Return: 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
