8dd8d42263660e5929beb92fb1087d0c9543a907
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 13:53:09 -0700

    line_

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,20 +8,12 @@
 
 void print_line(int n)
 {
-	int a = 0;
-	
-	if (n <= 0)
-	{
-		_putchar('\n');
-		break;
-	}
-	else
-	{
-		while (a < n)
-		{
-			_putchar('_');
-			a++;
-		}
-		_putchar('\n');
-	}
+int i = 0;
+
+if (n > 0)
+{
+for (; i < n; i++)
+_putchar('_');
+}
+_putchar('\n');
 }
