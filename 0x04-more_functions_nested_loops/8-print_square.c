8c225895e61f609da124596ea18633c30a886149
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 14:36:42 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - makes a square
+ * @size: size parameter
+ * Return: 0 success
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+		break;
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
