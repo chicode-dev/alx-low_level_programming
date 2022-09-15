e46fae36a95fd73ef2ae17cfd509a3b0d4bfe2d7
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 14:31:17 -0700

    lots of changes

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,26 +8,18 @@
 
 void print_diagonal(int n)
 {
-	int a = 0;
-	int b = 0;
+int i = 0, j;
 
-	if (n > 0)
-	{
-		while (a < n)
-		{
-			while (b < a)
-			{
-				_putchar(' ');
-				b++;
-			}
-			_putchar(92);
-			_putchar('\n');
-			a++;
-		}
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (n > 0)
+{
+for (; i < n; i++)
+{
+for (j = 0; j < i; j++)
+_putchar(' ');
+_putchar(92);
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
 }
