34f755357edf94381426bf4186da8b6b4c071e4a
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 13:47:37 -0700

    line_

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints a _ line
+ * @n: int parameter
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int a = 0;
+	
+	if (n <= 0)
+	{
+		_putchar('\n');
+		break;
+	}
+	else
+	{
+		while (a < n)
+		{
+			_putchar('_');
+			a++
+		}
+		_putchar('\n');
+	}
+}
