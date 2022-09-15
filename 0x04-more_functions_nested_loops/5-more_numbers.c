13a875eae21d3606d2a589fd50a86edfe1b21972
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 09:57:45 -0700

    0to14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - 0 to 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	for (j = 48; j <= 52; j++)
+	{
+		_putchar('1' + j);
+	}
+	_putchar('\n');
+}
