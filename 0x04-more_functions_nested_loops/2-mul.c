5030a1a50737df672b5fa913718bad94ebb05ef0
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 09:11:24 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 numbers
+ * @a: first int parameter
+ * @b: second int parameter
+ * Return: the multiplied answer
+ */
+
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
