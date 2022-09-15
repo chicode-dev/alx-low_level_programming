c40dfe037a630035c6dcdc764df39b9f938ac470
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-15 14:10:17 -0700

    diagline_

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 {
 	int a = 0;
 	int b = 0;
-	
+
 	if (n > 0)
 	{
 		while (a < n)
@@ -19,11 +19,10 @@ void print_diagonal(int n)
 			{
 				_putchar(' ');
 				b++;
-			} 
+			}
 			_putchar(92);
 			_putchar('\n');
 			a++;
-			b = 0;
 		}
 		_putchar('\n');
 	}
