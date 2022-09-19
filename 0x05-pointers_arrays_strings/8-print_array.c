84dbfaf7e9dea9e9c3fba81075254820903676d0
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-19 13:55:46 -0700

    everyother

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-/** 
+/**
  * print_array - just print an array
  * @a: array parameter
  * @n: amount to print
@@ -13,7 +13,7 @@ void print_array(int *a, int n)
 	int i = 0;
 
 	printf("%d", a[0]);
-	for (; i < n; i++)
+	for (; i < n - 1; i++)
 	{
 		printf(", %d", a[i + 1]);
 	}
