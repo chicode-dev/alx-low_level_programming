d11d290c2f890c9e1bc44c6c8dc7ff70cdda0351
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-19 05:33:52 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: string parameter
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		/* increase number */
+		count++
+		/* increase char value */
+		s++
+	}
+	return count;
+}
