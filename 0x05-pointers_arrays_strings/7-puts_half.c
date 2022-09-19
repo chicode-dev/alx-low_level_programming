ce80a2b4f31482653e371de4609794728c5c120b
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-19 13:23:12 -0700

    halfstring

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - half a string
+ * @str: parameter string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int count;
+	int i;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		i = count / 2;
+	}
+	else
+	{
+		i = (count - 1)/2;
+	}
+	for (i++; i < count; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
