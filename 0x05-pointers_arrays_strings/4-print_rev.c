552f5a7c64f899e307e989f29553879f7d5ababa
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-19 07:44:37 -0700

    reverseorder

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: char parameter
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (int i = count - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
