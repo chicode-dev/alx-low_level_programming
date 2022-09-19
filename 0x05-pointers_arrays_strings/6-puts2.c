f847dd1313e9bc90babb595ea953314b4909897b
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-19 12:17:12 -0700

    everyother

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts2 - string with new line
+ * @str: parameter string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		if (n % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
+		n++;
+	}
+	_putchar('\n');
+}
