876d03b09586f459f61004e46d3ad22f158af53e
Chimdindu-dev <cchimdindu@gmail.com>
2022-09-22 12:13:27 -0700

    check

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *@s1: par1
+ *@s2: par2
+ *Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+char *str_one = s1;
+char *str_two = s2;
+
+
+while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
+{
+str_one++;
+str_two++;
+}
+
+return (*str_one - *str_two);
+}
