fd0a89830dccea40bd72535228d54470ca2f9ce1
Chimdindu-dev <cchimdindu@gmail.com>
2022-10-10 06:49:44 -0700

    header for struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * struct dog - dog variables
+ * @name: para
+ * @age: para
+ * @owner: para
+ *
+ * Description: simple dog struct
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
