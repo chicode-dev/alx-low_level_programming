5b00abd1cbe0cd509d416bf3a490d68d41791ac9
Chimdindu-dev <cchimdindu@gmail.com>
2022-10-12 11:18:40 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search for int
+ * @array: para array
+ * @size: int para
+ * @cmp: pointer fun
+ * Return: single int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (array && cmp)
+{
+	if (size <= 0)
+		return (-1);
+	else
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]) != 0)
+				return (i);
+}
+return (-1);
+}
