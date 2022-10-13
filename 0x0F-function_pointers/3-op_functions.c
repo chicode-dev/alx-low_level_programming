93cdfa78061aa6dd85573aa46291fbd7364be5de
Chimdindu-dev <cchimdindu@gmail.com>
2022-10-12 18:01:14 -0700

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,81 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds 2 numbers
+ * @a: int para
+ * @b: int para
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	return add;
+}
+
+/**
+ * op_add - subtract 2 numbers
+ * @a: int para
+ * @b: int para
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	int sub;
+
+	sub = a - b;
+	return sub;
+}
+
+/**
+ * op_add - mulitple 2 numbers
+ * @a: int para
+ * @b: int para
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return mul;
+}
+
+/**
+ * op_add - divide 2 numbers
+ * @a: int para
+ * @b: int para
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	int div;
+	
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	div = a / b;
+	return div;
+}
+
+/**
+ * op_add - mod 2 numbers
+ * @a: int para
+ * @b: int para
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	int mod;
+
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	mod = a % b;
+	return mod;
+}
